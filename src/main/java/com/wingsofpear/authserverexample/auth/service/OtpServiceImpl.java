c691cec6bfd747d3792cc2fd29f2484ba74b09a7
Kelvin Leung <kwleungaj@gmail.com>
2025-05-27 23:50:13 -0400

    refactor code

--- src/main/java/com/wingsofpear/authserverexample/auth/service/OtpServiceImpl.java
+++ src/main/java/com/wingsofpear/authserverexample/auth/service/OtpServiceImpl.java
@@ -1,5 +1,6 @@
 package com.wingsofpear.authserverexample.auth.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.util.Assert;
@@ -10,8 +11,12 @@ import static com.wingsofpear.authserverexample.common.util.OtpUtil.generateOtp;
 
 @Service
 public class OtpServiceImpl implements OtpService {
+    @Value("${app.auth.otp.ttl-minutes}")
+    private int TTL_MINUTES;
+    @Value("${app.auth.otp.code-length}")
+    private int CODE_LENGTH;
+    private final Duration OTP_VALIDITY = Duration.ofMinutes(TTL_MINUTES);
     private final RedisTemplate<String, String> redisTemplate;
-    private final Duration OTP_VALIDITY = Duration.ofMinutes(5);
 
     public OtpServiceImpl(RedisTemplate redisTemplate) {
         this.redisTemplate = redisTemplate;
@@ -26,7 +31,7 @@ public class OtpServiceImpl implements OtpService {
     @Override
     public String requestOtp(String email) {
         Assert.hasText(email, "email cannot be empty");
-        String otp = generateOtp(6);
+        String otp = generateOtp(CODE_LENGTH);
         redisTemplate.opsForValue().set(email, otp, OTP_VALIDITY);
         return otp;
     }
